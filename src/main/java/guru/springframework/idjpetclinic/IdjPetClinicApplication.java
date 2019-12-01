0eb241e0226337e676e6f6aa95ead696b55d4ce9
djuric-ivan <djuricivan43@gmail.com>
2019-12-01 22:53:33 +0100

    Inital Project commit

---
+++ src/main/java/guru/springframework/idjpetclinic/IdjPetClinicApplication.java
@@ -0,0 +1,12 @@
+package guru.springframework.idjpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdjPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdjPetClinicApplication.class, args); }
+
+}
---
+++ src/test/java/guru/springframework/idjpetclinic/IdjPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.idjpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdjPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
