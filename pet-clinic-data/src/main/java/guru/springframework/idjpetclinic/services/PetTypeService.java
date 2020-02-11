86fff1b53d31d13ee8e08f51415d0546d6946a88
djuric-ivan <djuricivan43@gmail.com>
2020-02-11 22:07:27 +0100

    added PetTypeService

---
+++ pet-clinic-data/src/main/java/guru/springframework/idjpetclinic/services/PetTypeService.java
@@ -0,0 +1,7 @@
+package guru.springframework.idjpetclinic.services;
+
+import guru.springframework.idjpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+}
